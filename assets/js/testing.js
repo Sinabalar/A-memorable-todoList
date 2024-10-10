a7287088f190525362fb8f06795b65669168fdf1
Sinabalar <sinabalar666@gmail.com>
2024-10-11 02:27:16 +0330

    Completion after 4 years ^^

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -1,31 +1,68 @@
-function getItem() {
+const input = document.getElementById('input');
+input.addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+            getItem()
+        }
+    }
+);
 
-    var inputData;
-    inputData = document.getElementById('input').value;
-    if (inputData == "") {
+function getItem() {
+    if (!input.value) {
         alert('please enter a task');
     } else {
-        document.getElementById('input').value = ''
-        var liTag = document.createElement('li');
-        var tasktag = document.createElement('span');
-        var task = document.createTextNode(inputData);
-        tasktag.appendChild(task)
-        liTag.appendChild(tasktag);
-        var result = document.getElementById('result');
+        let liTag = document.createElement('li');
+        let taskTag = document.createElement('span');
+        let task = document.createTextNode(input.value);
+        taskTag.appendChild(task)
+        liTag.appendChild(taskTag);
+        let result = document.getElementById('result');
         result.appendChild(liTag);
-        var remTag = document.createElement('button');
-        var iTag = document.createElement('i');
+        let remTag = document.createElement('button');
+        let iTag = document.createElement('i');
         remTag.appendChild(iTag);
-        iTag.setAttribute('class','fas fa-trash' );
+        iTag.setAttribute('class', 'fas fa-trash');
         liTag.appendChild(remTag);
-        liTag.addEventListener("click", taskDone);
+        input.value = '';
 
-        function taskDone() {
-            tasktag.style.textDecoration = "line-through";
+
+        function taskDone(calledByUpdatePendingText) {
+            !calledByUpdatePendingText && (taskTag.classList.toggle('task-done'))
+
+            const allTasks = Array.from(document.querySelectorAll('li span'));
+
+            const completedTasks = allTasks.filter(el => el.classList.contains('task-done'));
+
+
+            if (!completedTasks.length) {
+                document.querySelector('.pending span').textContent = ``;
+            } else if (allTasks.length !== completedTasks.length) {
+                document.querySelector('.pending span').textContent = `You done ${completedTasks.length} of them. (${Math.round((completedTasks.length / allTasks.length) * 100)})%`;
+            } else {
+                document.querySelector('.pending span').textContent = `All done ✅`;
+            }
         }
 
-        var remove = document.createAttribute('onclick')
-        remove.value = 'this.parentNode.remove();';
-        remTag.setAttributeNode(remove)
+
+        function updatePendingText() {
+            taskDone(true)
+            const allTasks = document.querySelectorAll('li')
+            const pending = document.querySelector('.pending p');
+            pending.textContent = `You have ${allTasks.length} task${allTasks.length > 1 ? 's' : ''}`;
+        }
+
+
+        function handelRemove() {
+            this.parentNode.remove();
+            updatePendingText();
+            taskDone(false);
+        }
+
+
+        updatePendingText();
+
+        taskTag.addEventListener("click", () => taskDone(false));
+
+
+        remTag.addEventListener('click', handelRemove.bind(remTag));
     }
 }
